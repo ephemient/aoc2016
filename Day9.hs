ee51a136da7f4c2f9c3c2880c0b647330d990ab3
Daniel Lin <ephemient@gmail.com>
2016-12-09 01:37:01 -0500

    Day 9: Explosives in Cyberspace
    http://adventofcode.com/2016/day/9

---
+++ Day9.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE BangPatterns, ViewPatterns #-}
+module Day9 (main) where
+
+import Data.Char (isDigit, isSpace)
+import Data.List (genericDrop, genericLength)
+import System.IO.Unsafe (unsafePerformIO)
+
+input :: String
+input = filter (not . isSpace) $ unsafePerformIO $ readFile "day9.txt"
+
+v1 :: (Integral a, Read a) => String -> a
+v1 = v1' 0 where
+    v1' k [] = k
+    v1' !k ('(' :
+            (span isDigit ->
+             (read -> len, 'x' :
+              (span isDigit ->
+               (read -> count, ')' :
+                (genericDrop len -> s))))))
+      = v1' (k + count * len) s
+    v1' !k (span (/= '(') -> (genericLength -> c, s)) = v1' (k + c) s
+
+v2 :: (Integral a, Read a) => String -> a
+v2 = v2' 0 where
+    v2' k [] = k
+    v2' !k ('(' :
+            (span isDigit ->
+             (read -> len, 'x' :
+              (span isDigit ->
+               (read -> count, ')' :
+                (splitAt len ->
+                 (v2' 0 -> c, s)))))))
+      = v2' (k + count * c) s
+    v2' !k (span (/= '(') -> (genericLength -> c, s)) = v2' (k + c) s
+
+main :: IO ()
+main = do
+    print $ v1 input
+    print $ v2 input
